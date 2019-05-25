9472c170685be6cff7661d70014f3d3b8a1c5be7
Frank Mortensen <fmortens@me.com>
2019-05-25 17:47:25 +0200

    Set up chat screen with keyboard handling

---
+++ frankchat/View/Chat/ChatViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ChatViewController.swift
+//  frankchat
+//
+//  Created by Frank Mortensen on 24/05/2019.
+//  Copyright © 2019 Frank Mortensen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ChatViewController: UIViewController, UITextFieldDelegate {
+    
+    var chat: Chat?
+    
+    @IBOutlet weak var chatTableView: UITableView!
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        if let chat = chat {
+            print("Chat view opened \(chat)")
+        }
+        
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        self.view.addGestureRecognizer(
+            UITapGestureRecognizer(
+                target: self.view,
+                action: #selector(UIView.endEditing(_:))
+            )
+        )
+        
+        textField.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        
+        if let chat = self.chat, let messageToSend = self.textField.text {
+            
+            
+            if chat.id != nil {
+                print("CONTINUE CHAT \(messageToSend)")
+            
+            } else {
+                print("NEW CHAT \(messageToSend)")
+            }
+        
+            
+        }
+        
+        return true
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+}
