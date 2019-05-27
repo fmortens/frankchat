7e3bb5bed513341aba775ab06490f18a6f2af52b
Frank Mortensen <fmortens@me.com>
2019-05-27 20:39:25 +0200

    Update code somewhat. Removed unused stuff etc.

--- frankchat/View/Chat/ChatListViewController.swift
+++ frankchat/View/Chat/ChatListViewController.swift
@@ -15,7 +15,7 @@ class ChatListViewController: UIViewController, UITableViewDelegate, UITableView
     @IBOutlet weak var logoutButton: UIBarButtonItem!
     @IBOutlet var chatListView: UITableView!
     
-    var chats = [Conversation]()
+    var conversations = [Conversation]()
     var conversation: Conversation?
     
     override func viewDidLoad() {
@@ -31,31 +31,31 @@ class ChatListViewController: UIViewController, UITableViewDelegate, UITableView
         
         if (changeType == .added) {
             
-            let chat = Conversation(
+            let conversation = Conversation(
                 id: change.document.documentID,
                 participants: change.document.data()["participants"] as! [String],
                 updated: change.document.data()["updated"] as? Timestamp
             )
             
-            print("Added \(chat)")
+            print("Added \(conversation)")
             
-            self.chats.append(chat)
+            self.conversations.insert(conversation, at: 0)
         }
         
         if (changeType == .modified) {
             
-            let chat = Conversation(
+            let conversation = Conversation(
                 id: change.document.documentID,
                 participants: change.document.data()["participants"] as! [String],
                 updated: change.document.data()["updated"] as? Timestamp
             )
             
-            self.chats[Int(change.oldIndex)] = chat
+            self.conversations[Int(change.oldIndex)] = conversation
             
         }
         
         if (changeType == .removed) {
-            self.chats.remove(at: Int(change.oldIndex))
+            self.conversations.remove(at: Int(change.oldIndex))
         }
         
         self.chatListView.reloadData()
@@ -64,17 +64,17 @@ class ChatListViewController: UIViewController, UITableViewDelegate, UITableView
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return self.chats.count
+        return self.conversations.count
         
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let chat = chats[indexPath.row]
+        let conversation = conversations[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell")!
         
-        cell.textLabel!.text = "From: \(chat.participants[0])"
+        cell.textLabel!.text = "From: \(conversation.participants[0])"
         
         return cell
         
@@ -82,29 +82,11 @@ class ChatListViewController: UIViewController, UITableViewDelegate, UITableView
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        self.conversation = chats[indexPath.row]
-        
-//        FirebaseClient.getLatestChatMessage(documentId: chat.id) { (success, snapshot) in
-//
-//            guard let success = success else {
-//                print("FAILURE")
-//                return
-//            }
-//
-//            if success {
-//                print("successful")
-//
-//                if let snapshot = snapshot {
-//
-//                    for document in snapshot.documents {
-//                        print("document is -> \(document.data()))")
-//                    }
-//                }
-//            }
-//
-//        }
+        self.conversation = conversations[indexPath.row]
         
         self.performSegue(withIdentifier: "PresentChatView", sender: nil)
+        
+        tableView.deselectRow(at: indexPath, animated: true)
         
     }
     
